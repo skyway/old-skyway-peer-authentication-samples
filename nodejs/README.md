0d3d4ff610faa82c3ba5173bb94b9f0061617ade
Alan Iida <alan.m.shelly@gmail.com>
2017-04-19 14:43:33 +0900

    add nodejs sample

---
+++ nodejs/README.md
@@ -0,0 +1,14 @@
+# NodeJS Authentication Sample Server
+
+## Set up
+
+- install [nodejs](https://nodejs.org/)
+- run `npm install` to install required packages
+
+## Run the server
+
+```bash
+$ node sample.js
+```
+
+You can now post to `http://localhost:8080/authentication` to generate a credential.
---
+++ nodejs/sample.js
@@ -0,0 +1,74 @@
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const hmac = require('crypto-js/hmac-sha256');
+const CryptoJS = require('crypto-js');
+
+/************************************************
+ *            Config section start              *
+ *         replace with your own values         *
+ ************************************************/
+
+const secretkey = 'YourSecretKey'; // replace with your own secretkey from the dashboard
+const credentialTTL = 3600; // 1 hour
+
+/************************************************
+ *            Config section finished           *
+ ************************************************/
+
+const app = express();
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.post('/authenticate', (req, res) => {
+  const peerId = req.body.peerId;
+  const sessionToken = req.body.sessionToken;
+
+  checkSessionToken(peerId, sessionToken).then(() => {
+    // Session token check was successful.
+
+    // We need the current unix timestamp. Date.now() returns in milliseconds so divide by 1000 to get seconds.
+    const unixTimestamp = Math.floor(Date.now() / 1000);
+
+    const credential = {
+      peerId: peerId,
+      timestamp: unixTimestamp,
+      ttl: credentialTTL,
+      authToken: calculateAuthToken(peerId, unixTimestamp)
+    };
+
+    res.send(credential);
+  }).catch(() => {
+    // Session token check failed
+    res.status(401).send('Authentication Failed');
+  });
+});
+
+const listener = app.listen(process.env.PORT || 8080, () => {
+  console.log(`Server listening on port ${listener.address().port}`)
+});
+
+function checkSessionToken(peerId, token) {
+  return new Promise((resolve, reject) => {
+    // Implement checking whether the session is valid or not.
+    // Resolve if the session token is valid.
+    // Reject if it is invalid.
+
+    resolve();
+  });
+}
+
+function calculateAuthToken(peerId, timestamp) {
+  // calculate the auth token hash
+  const hash = CryptoJS.HmacSHA256(`${timestamp}:${credentialTTL}:${peerId}`, secretkey);
+
+  // convert the hash to a base64 string
+  return CryptoJS.enc.Base64.stringify(hash);
+}
