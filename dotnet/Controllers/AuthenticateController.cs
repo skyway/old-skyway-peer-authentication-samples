8804bbe88f01bfd6eae15a757874886ce5e7b467
kadoshita <sublimer.me@gmail.com>
2021-10-16 18:48:17 +0900

    Add dotnet core sample

---
+++ dotnet/Controllers/AuthenticateController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Security.Cryptography;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace dotnet.Controllers
+{
+    public class PostBody
+    {
+        [Required]
+        public String peerId { get; set; }
+
+        [Required]
+        public string sessionToken { get; set; }
+    }
+
+    public class Authenticate
+    {
+        public String peerId { get; set; }
+
+        public double timestamp { get; set; }
+
+        public int ttl { get; set; }
+
+        public string authToken { get; set; }
+    }
+
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthenticateController : ControllerBase
+    {
+        private readonly ILogger<AuthenticateController> _logger;
+
+        /************************************************
+         *            Config section start              *
+         *         replace with your own values         *
+         ************************************************/
+        private readonly string secretKey = "YourSecretKey"; // replace with your own secretKey from the dashboard
+        private readonly int credentialTTL = 3600; // 1 hour
+
+        /************************************************
+         *            Config section finished           *
+         ************************************************/
+
+        public AuthenticateController(ILogger<AuthenticateController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public ActionResult<Authenticate> Post([FromForm] PostBody body)
+        {
+            if (checkSessionToken(body.peerId, body.sessionToken))
+            {
+                var unixTimestamp = Math.Floor(DateTime.UtcNow.Subtract(DateTime.UnixEpoch).TotalSeconds);
+                return new Authenticate()
+                {
+                    peerId = body.peerId,
+                    timestamp = unixTimestamp,
+                    ttl = credentialTTL,
+                    authToken = calculateAuthToken(body.peerId, unixTimestamp)
+                };
+            }
+            else
+            {
+                return StatusCode(401);
+            }
+        }
+
+        private Boolean checkSessionToken(string peerId, string token)
+        {
+            // Implement checking whether the session is valid or not.
+            // Return true if the session token is valid.
+            // Return false if it is invalid.
+            return true;
+        }
+
+        private string calculateAuthToken(string peerId, double timestamp)
+        {
+            using (HMACSHA256 hmac = new HMACSHA256(Encoding.UTF8.GetBytes(secretKey)))
+            {
+                var sign = $"{timestamp.ToString()}:{credentialTTL.ToString()}:{peerId}";
+                var hash = hmac.ComputeHash(Encoding.UTF8.GetBytes(sign));
+                return Convert.ToBase64String(hash);
+            }
+        }
+    }
+}
---
+++ dotnet/Startup.cs
@@ -0,0 +1,57 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+
+namespace dotnet
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "dotnet", Version = "v1" });
+            });
+            services.AddCors(options=>{
+                options.AddDefaultPolicy(builder =>
+                    builder.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .WithHeaders("Origin", "X-Requested-With", "Content-Type", "Accept"));
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "dotnet v1"));
+            }
+
+            app.UseRouting();
+
+            app.UseCors();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
